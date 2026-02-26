6212f4afbcb6329ae6f4fe22f7707f29d5102c41
ShishkaDanil <70168790+ShishkaDanil@users.noreply.github.com>
2026-02-26 15:07:25 +0300

    refactor: email sending

--- kei_backend/settings.py
+++ kei_backend/settings.py
@@ -205,9 +205,11 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST', default='localhost')
 EMAIL_PORT = config('EMAIL_PORT', cast=int, default=587)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=False)
 EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool, default=False)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_TIMEOUT = config('EMAIL_TIMEOUT', cast=int, default=10)
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@example.com')
 
 LOGGING = {
